0bb41ce6466751918a78ea37c4b46ce5f9212a9b
Sabbha20 <sabbha.power1@gmail.com>
2024-09-30 21:40:22 +0530

    media, static and migration completed

--- facerecognition/admin.py
+++ facerecognition/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import FaceRecognition
 # Register your models here.
+
+@admin.register(FaceRecognition)
+class FaceRecognitionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'image', 'record_date')
--- mlwd/urls.py
+++ mlwd/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 # from facerecognition import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path("", views.index, name="facerecognition"),
     path("", include('facerecognition.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
