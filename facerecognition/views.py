8a8b7e76759b19f3b5015837c5f799455433c351
Sabbha20 <sabbha.power1@gmail.com>
2024-10-01 08:13:04 +0530

    ML Pipeline added to FaceRecognition

--- facerecognition/views.py
+++ facerecognition/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from facerecognition.forms import FaceRecognitionForm
+from facerecognition.ml_pipeline import pipeline_model
+
+from django.conf import settings
+from facerecognition.models import FaceRecognition
+
+import os
 
 # Create your views here.
 def index(req):
@@ -8,8 +14,26 @@ def index(req):
     if req.method == "POST":
         form = FaceRecognitionForm(req.POST or None, req.FILES or None)
         if form.is_valid():
-            form.save(commit=True)
+            save = form.save(commit=True)
+            
+            # extract image from db
+            primary_key = save.pk
+            imgobj = FaceRecognition.objects.get(pk=primary_key)
+            fileroot = str(imgobj.image)
+            filepath = os.path.join(settings.MEDIA_ROOT, fileroot)
+            results = pipeline_model(filepath)
+            
+            print(results)
+            context = {
+                "form":form,
+                "upload": True,
+                "results": results, 
+            }
+            return render(req, 'index.html', context=context)
+            
+            
     context = {
         "form":form,
+        "upload": False,
     }
     return render(req, 'index.html', context=context)
